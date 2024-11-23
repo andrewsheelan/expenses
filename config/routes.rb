066a187193ee693b8ea4c617d9ee6ce313fcdfca
Andrew Paliyan <andrew.paliyan@bistromd.com>
2024-11-22 20:08:30 -0500

    Initial App files

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@ Rails.application.routes.draw do
     mount ActionCable.server => '/cable'
     root 'dashboard#index', as: :authenticated_root
   end
-  root 'devise/sessions#new'
   
   resources :expenses
   resources :categories
   resources :conversations, only: [:index, :show, :create] do
     resources :messages, only: [:create]
   end
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  root to: redirect('/users/sign_up')
 end
